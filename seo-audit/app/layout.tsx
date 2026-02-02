61ac4b8ef4a5752d2787365ff8b9436974d71c15
Rusudan Tsiskreli <140479498+tsiskrelir@users.noreply.github.com>
2026-02-02 23:40:50 +0400

    Update layout.tsx

--- seo-audit/app/layout.tsx
+++ seo-audit/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ka">
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
